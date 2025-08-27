9ef899db9f7899c04830bd8d4df2faa398dc412f
ajaygouda <ajaygouda10@gmail.com>
2025-08-27 23:32:14 +0530

    fixed header and footer

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -1,6 +1,7 @@
 "use client"
 import Link from 'next/link'
 import { useMenu } from '../api/context/menu-context';
+import { useState } from 'react';
 const PORT = process.env.NEXT_PUBLIC_API_URL;
 
 const socials = [
@@ -10,8 +11,24 @@ const socials = [
     { name: "insta", d: "M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2m-.2 2A3.6 3.6 0 0 0 4 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6C20 5.61 18.39 4 16.4 4H7.6m9.65 1.5a1.25 1.25 0 0 1 1.25 1.25A1.25 1.25 0 0 1 17.25 8 1.25 1.25 0 0 1 16 6.75a1.25 1.25 0 0 1 1.25-1.25M12 7a5 5 0 0 1 5 5 5 5 0 0 1-5 5 5 5 0 0 1-5-5 5 5 0 0 1 5-5m0 2a3 3 0 0 0-3 3 3 3 0 0 0 3 3 3 3 0 0 0 3-3 3 3 0 0 0-3-3z" },
     { name: "youtube", d: "M10 15l5.19-3L10 9v6m11.56-7.83c.13.47.22 1.1.28 1.9.07.8.1 1.49.1 2.09L22 12c0 2.19-.16 3.8-.44 4.83-.25.9-.83 1.48-1.73 1.73-.47.13-1.33.22-2.65.28-1.3.07-2.49.1-3.59.1L12 19c-4.19 0-6.8-.16-7.83-.44-.9-.25-1.48-.83-1.73-1.73-.13-.47-.22-1.1-.28-1.9-.07-.8-.1-1.49-.1-2.09L2 12c0-2.19.16-3.8.44-4.83.25-.9.83-1.48 1.73-1.73.47-.13 1.33-.22 2.65-.28 1.3-.07 2.49-.1 3.59-.1L12 5c4.19 0 6.8.16 7.83.44.9.25 1.48.83 1.73 1.73z" }
 ]
-export const Footer = ({ data }) => {
+export const Footer = () => {
     const { activeMenu } = useMenu();
+    const [data, setData] = useState()
+
+    useEffect(() => {
+        const fetchFooter = async () => {
+            try {
+                const footerRes = await fetch(`${PORT}/api/footer?populate[logo]=true&populate[socials]=true&populate[menus]=true&populate[address][populate][emails]=true&populate[address][populate][phones]=true`);
+                const res = await footerRes.json();
+                setData(res)
+            }
+            catch (err) {
+
+            }
+        }
+        fetchFooter();
+    }, [])
+
     return (
         <div className="border-t-1 border-t-gray-200 dark:border-t-gray-500">
             <div className='container-1256'>
